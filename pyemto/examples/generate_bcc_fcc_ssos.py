42c4da38258f65d35f11c05dfba363f092d2612c
hpleva <hpleva@utu.fi>
2016-03-19 20:32:43 +0200

    fixes

---
+++ pyemto/examples/generate_bcc_fcc_ssos.py
@@ -0,0 +1,38 @@
+from emto_input_generator import *
+import numpy as np
+
+folder = os.getcwd()           # Get current working directory.
+emtopath = folder+"/bcc_fcc_ssos"  # Folder where the calculations will be performed.
+latpath = emtopath
+
+# bcc SSOS-1
+prims = np.array([[0.0,2.0,3.0],
+                  [-0.5,2.5,2.5],
+                  [-0.5,-0.5,0.5]])
+
+basis = np.array([[0.0,0.0,0.0],
+                  [0.6,0.6,0.4],
+                  [0.4,0.4,0.6],
+                  [0.2,0.2,0.8],
+                  [0.8,0.8,0.2]])
+
+species = ["Co","Cr","Fe","Mn","Ni"]
+species_cpa = species
+
+input_creator = EMTO(folder=emtopath)
+
+input_creator.init_structure(latpath=latpath,
+                             prims=prims,
+                             basis=basis,
+                             atoms=species,
+                             latname='ssos_bcc_1')
+
+input_creator.init_bulk(atoms_cpa=species_cpa)
+
+sws_range = np.linspace(2,3,6)
+
+input_creator.write_bmdl_kstr_shape_input()
+#input_creator.write_kgrn_kfcd_input()
+input_creator.write_kgrn_kfcd_swsrange(sws=sws_range)
+
+input_creator.draw_structure('output')
