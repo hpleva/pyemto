0a35583fdfa36904c1abff28f91e49f13b9d418c
hpleva <hpleva@utu.fi>
2017-10-18 10:54:33 +0300

    Update alloy_discovery.py to the latest version that was used in Titan cluster.

--- pyemto/examples/alloy_discovery/alloy_discovery.py
+++ pyemto/examples/alloy_discovery/alloy_discovery.py
@@ -2,7 +2,12 @@
 
 import numpy as np
 import os
+
+# Help python to find the pyemto folder
+#import sys
+#sys.path.insert(0, "/home/hpleva/pyemto")
 import pyemto
+
 
 ##################################################################
 ##################################################################
@@ -12,8 +17,8 @@ import pyemto
 ##################################################################
 ##################################################################
 
-EMTOdir = "/home/hpleva/local_emto_stuff/openmp-stable-cmake"
-latpath = "/home/hpleva/local_emto_stuff/structures" # Path to bmdl, kstr and shape directories
+EMTOdir = "/home/hpleva/EMTO/openmp-stable-cmake"
+latpath = "/home/hpleva/structures" # Path to bmdl, kstr and shape directories
 base_dir = os.getcwd()
 
 # CoCrFeMnNi
@@ -30,8 +35,8 @@ for i in range(len(systems[0])):
         if i > 0 and j == 0:
             pass
         else:
-            conc = concs_per_cent[j] / 100
-            conc_rest = (1.0 - conc) / 4
+            conc = concs_per_cent[j] #/ 100
+            conc_rest = (100.0 - conc) / 4
             if i == 0:
                 concentrations.append([conc,conc_rest,conc_rest,conc_rest,conc_rest])
             if i == 1:
@@ -43,11 +48,18 @@ for i in range(len(systems[0])):
             if i == 4:
                 concentrations.append([conc_rest,conc_rest,conc_rest,conc_rest,conc])
 
-# Possible NM (Non-magnetic), FM (ferromagnetic) and
+# Pure elements
+concentrations.append([100.0, 0, 0, 0, 0])
+concentrations.append([0, 100.0, 0, 0, 0])
+concentrations.append([0, 0, 100.0, 0, 0])
+concentrations.append([0, 0, 0, 100.0, 0])
+concentrations.append([0, 0, 0, 0, 100.0])
+
+# Possible NM (Non-magnetic), FM (ferromagnetic) and 
 # DLM (Disordered local moments).
 #magn = "NM"
 #magn = "FM"
-magn = "DLM"
+magn = "DLM" 
 
 initial_sws = 2.61
 
@@ -74,8 +86,8 @@ else:
 if not len(initial_sws) == 3:
     print("ERROR: intial_sws should be a float or list of 3 floats!")
     exit()
-
-
+            
+            
 # Sanity checks
 for s in systems:
     if not len(s) == len(systems[0]):
@@ -166,17 +178,17 @@ if mode == 'create_inputs':
     for si in range(len(systems)):
         s = systems[si]
         split = splits[si]
-        # Create main directory
+        # Create main directory 
         sname = ""
         if magn == "DLM":
-            nlist = [s[i] for i in range(0,len(s),2)]
+            nlist = [s[i] for i in range(0,len(s),2)]            
         else:
             nlist = s
         #nlist = s
         for atom in nlist:
             sname = sname + atom
-
-        #
+        
+        # 
         # Make directories
         if not os.path.lexists(sname):
             os.makedirs(sname)
@@ -194,7 +206,7 @@ if mode == 'create_inputs':
             for conc in clist:
                 count += 1
                 cname = cname +str(int(conc*1000)).zfill(4)
-
+                
                 if not count == len(clist):
                     cname = cname+"-"
 
@@ -208,11 +220,11 @@ if mode == 'create_inputs':
                 os.makedirs(apath)
 
             # Copy QNA parameter file to each folder
-            #os.system("cp ./QNA_database.dat {0}".format(apath))
-
+            os.system("cp ./QNA_database.dat {0}".format(apath))
+            
             # Construct base jobname
             jobname = ""
-
+            
             for i in range(len(nlist)):
                 if jobname == "":
                     pass
@@ -239,9 +251,13 @@ if mode == 'create_inputs':
                        xc='PBE',
                        runtime="12:00:00",
                        stmp='A',
-                       niter=300)#,
+                       niter=300,
+                       tole=1.0e-8,
+                       nky=31,
+                       alpcpa=0.796)#,
                        #runKGRN=False)
-            swsrange = np.linspace(initialsws-0.1,initialsws+0.1,7) # A list of 7 different volumes
+            #swsrange = np.linspace(initialsws-0.1,initialsws+0.1,7) # A list of 7 different volumes
+            swsrange = np.linspace(2.40,2.80,20)
             alloy.lattice_constants_batch_generate(sws=swsrange)
 
             # FCC second
@@ -263,9 +279,17 @@ if mode == 'create_inputs':
                        xc='PBE',
                        runtime="12:00:00",
                        stmp='A',
-                       niter=300)#,
+                       niter=300,
+                       nky=25,
+                       tole=1.0e-8,
+                       alpcpa=0.796)#,
                        #runKGRN=False)
-            swsrange = np.linspace(initialsws-0.1,initialsws+0.1,7) # A list of 7 different volumes
+            #swsrange = np.linspace(initialsws-0.1,initialsws+0.1,7) # A list of 7 different volumes
+            # FM
+            #swsrange = np.linspace(2.56,2.66,15)
+            #swsrange = np.linspace(2.56,2.76,29)
+            # DLM
+            swsrange = np.linspace(2.40,2.80,20)
             alloy.lattice_constants_batch_generate(sws=swsrange)
 
             # HCP last
@@ -285,11 +309,15 @@ if mode == 'create_inputs':
                        expan='M',
                        sofc='Y',
                        xc='PBE',
-                       nky=11,
-                       nkz=7,
+                       nky=21,
+                       nkz=13,
                        stmp='A',
-                       niter=300)#,
-            swsrange = np.linspace(initialsws-0.1,initialsws+0.1,7) # A list of 7 different volumes
+                       niter=300,
+                       runtime='24:00:00',
+                       tole=1.0e-8,
+                       alpcpa=0.796)#,
+            #swsrange = np.linspace(initialsws-0.1,initialsws+0.1,7) # A list of 7 different volumes
+            swsrange = np.linspace(2.40,2.80,20)
             alloy.lattice_constants_batch_generate(sws=swsrange)
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -303,16 +331,16 @@ elif mode == 'compute_eq_energy':
     for si in range(len(systems)):
         s = systems[si]
         split = splits[si]
-        # Create main directory
+        # Create main directory 
         sname = ""
         if magn == "DLM":
-            nlist = [s[i] for i in range(0,len(s),2)]
+            nlist = [s[i] for i in range(0,len(s),2)]            
         else:
             nlist = s
         #nlist = s
         for atom in nlist:
             sname = sname + atom
-
+        
         for c in concentrations:
             sc_res = []
             # Make subdirectory for concentration
@@ -327,7 +355,7 @@ elif mode == 'compute_eq_energy':
             for conc in clist:
                 count += 1
                 cname = cname +str(int(conc*1000)).zfill(4)
-
+                
                 if not count == len(clist):
                     cname = cname+"-"
 
@@ -342,7 +370,7 @@ elif mode == 'compute_eq_energy':
 
             # Construct base jobname
             jobname = ""
-
+            
             for i in range(len(nlist)):
                 if jobname == "":
                     pass
@@ -376,7 +404,8 @@ elif mode == 'compute_eq_energy':
                        xc='PBE',
                        runtime="12:00:00",
                        stmp='A',
-                       niter=300)#,
+                       niter=300,
+                       alpcpa=0.796)#,
                        #runKGRN=False)
             alloy.write_inputs()
 
@@ -405,7 +434,8 @@ elif mode == 'compute_eq_energy':
                        xc='PBE',
                        runtime="12:00:00",
                        stmp='A',
-                       niter=300)#,
+                       niter=300,
+                       alpcpa=0.796)#,
                        #runKGRN=False)
             alloy.write_inputs()
 
@@ -418,13 +448,13 @@ elif mode == 'compute_eq_energy':
                        latpath=latpath,
                        sws=initialsws,
                        atoms = s,
-                       concs = c,
+                       concs = c, 
                        xc='PBE')
-
+     
             swsrange = np.linspace(initialsws-0.1,initialsws+0.1,7) # A list of 7 different volumes
-            #alloy.lattice_constants_batch_generate(sws=swsrange)
+            #alloy.lattice_constants_batch_generate(sws=swsrange)        
             sws0, c_over_a0, B0, e0, R0, cs0 = alloy.lattice_constants_analyze(sws=swsrange,prn=False)
-            alloy.sws = sws0
+            alloy.sws = sws0 
             ca = round(c_over_a0,3)
             hcpname ="hcp_"+str(ca) # Structure name
             structpath = "../"
@@ -441,7 +471,7 @@ elif mode == 'compute_eq_energy':
                 alloy.lattice.kstr.write_input_file(folder=structpath)
                 alloy.lattice.shape.write_input_file(folder=structpath)
                 alloy.lattice.batch.write_input_file(folder=structpath)
-
+                
 
             # Make kfcd and kgrn input files
             print("hcp", afm)
@@ -463,11 +493,12 @@ elif mode == 'compute_eq_energy':
                        sofc='Y',
                        xc='PBE',
                        nky=11,
-                       nkz=7)
+                       nkz=7,
+                       alpcpa=0.796)
 
             alloy.write_inputs()
-
-
+            
+           
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -480,16 +511,16 @@ elif mode == 'analyze_results':
     for si in range(len(systems)):
         s = systems[si]
         split = splits[si]
-        # Create main directory
+        # Create main directory 
         sname = ""
         if magn == "DLM":
-            nlist = [s[i] for i in range(0,len(s),2)]
+            nlist = [s[i] for i in range(0,len(s),2)]            
         else:
             nlist = s
         #nlist = s
         for atom in nlist:
             sname = sname + atom
-
+        
         for c in concentrations:
             sc_res = []
             # Make subdirectory for concentration
@@ -504,7 +535,7 @@ elif mode == 'analyze_results':
             for conc in clist:
                 count += 1
                 cname = cname +str(int(conc*1000)).zfill(4)
-
+                
                 if not count == len(clist):
                     cname = cname+"-"
 
@@ -519,7 +550,7 @@ elif mode == 'analyze_results':
 
             # Construct base jobname
             jobname = ""
-
+            
             for i in range(len(nlist)):
                 if jobname == "":
                     pass
@@ -528,6 +559,7 @@ elif mode == 'analyze_results':
                 jobname = jobname + nlist[i].lower() + "%4.2f" % (clist[i])
             finalname = jobname + "_final"
 
+            """
             # BCC first
             initialsws = initial_sws[0]
             alloy = pyemto.System(folder=apath,EMTOdir=EMTOdir)
@@ -552,14 +584,14 @@ elif mode == 'analyze_results':
             # get magnetic moments
             magn_moms = alloy.get_moments()
             # get total DOS at Fermi level
-            dos_total = alloy.get_fdos()
+            dos_total = alloy.get_fdos()            
             # Sanity checks
             if e_dft == None:
                 e_dft = 0.0
             if magn_moms == None:
                 magn_moms = [0 for i in range(len(concs))]
             sc_res.append([e_dft,sws0,B0,grun0,dos_total,e0,R_squared,magn_moms])
-
+     
             # FCC second
             initialsws = initial_sws[1]
             alloy = pyemto.System(folder=apath,EMTOdir=EMTOdir)
@@ -590,7 +622,8 @@ elif mode == 'analyze_results':
             if magn_moms == None:
                 magn_moms = [0 for i in range(len(concs))]
             sc_res.append([e_dft,sws0,B0,grun0,dos_total,e0,R_squared,magn_moms])
-
+            """
+            
             # HCP last
             initialsws = initial_sws[2]
             alloy = pyemto.System(folder=apath,EMTOdir=EMTOdir)
@@ -599,11 +632,11 @@ elif mode == 'analyze_results':
                        jobname=jobname, # hcp add automatically hcp string to jobname
                        latpath=latpath, sws=initialsws, atoms = s,
                        concs = c, xc='PBE')
-
+     
             swsrange = np.linspace(initialsws-0.1,initialsws+0.1,7) # A list of 7 different volumes
-            #alloy.lattice_constants_batch_generate(sws=swsrange)
-            sws0, c_over_a0, B0, e0, R0, cs0, grun0, R_squared = alloy.lattice_constants_analyze(sws=swsrange,prn=False,return_error=True)
-            alloy.sws = sws0
+            #alloy.lattice_constants_batch_generate(sws=swsrange)        
+            sws0, c_over_a0, B0, e0, R0, cs0, grun0, R_squared = alloy.lattice_constants_analyze(sws=swsrange,prn=True,return_error=True)
+            alloy.sws = sws0 
             ca = round(c_over_a0,3)
             hcpname ="hcp_"+str(ca) # Structure name
             alloy.bulk(lat='hcp', jobname=finalname+"_hcp",latpath=latpath, latname=hcpname,
@@ -628,7 +661,7 @@ elif mode == 'analyze_results':
 
 
     from pyemto.utilities.utils import wsrad_to_latparam
-
+    
     output_all = ""
 
     for r in results:
@@ -643,7 +676,7 @@ elif mode == 'analyze_results':
         output = "  System: "+sname+"\n"
         output = output + "    Magn: " + magn + "\n"
         output = output + "    Conc: " + conc_line + "\n"
-
+        
         bcc = r[1][0]
         bcc_lc = wsrad_to_latparam(bcc[1],'bcc')
         output = output + "#  Struc.   dft E(Ry)    lc(AA)    sws(bohr) B(GPa)     grun       DOSEf(1/eV)    fit E(Ry)   fit err      (c/a)\n"
@@ -653,7 +686,7 @@ elif mode == 'analyze_results':
         for i in range(len(bcc[7])):
             output = output + " {0:9.6f}".format(bcc[7][i])
         output = output + "\n"
-
+        
         fcc = r[1][1]
         fcc_lc = wsrad_to_latparam(fcc[1],'fcc')
         output = output + "     fcc: {0:13.6f} {1:9.6f} {2:9.6f} {3:11.6f} {4:8.6f} {5:10.6f} {6:13.6f} {7:12.10f} {8:8.6f}\n".format(fcc[0],fcc_lc,fcc[1],fcc[2],fcc[3],fcc[4],fcc[5],fcc[6],1.0)
@@ -662,7 +695,7 @@ elif mode == 'analyze_results':
         for i in range(len(fcc[7])):
             output = output + " {0:9.6f}".format(fcc[7][i])
         output = output + "\n"
-
+        
         hcp = r[1][2]
         hcp_lc = wsrad_to_latparam(hcp[1],'hcp',ca=hcp[8])
         output = output +"     hpc: {0:13.6f} {1:9.6f} {2:9.6f} {3:11.6f} {4:8.6f} {5:10.6f} {6:13.6f} {7:12.10f} {8:8.6f}\n".format(hcp[0],hcp_lc,hcp[1],hcp[2],hcp[3],hcp[4],hcp[5],hcp[6],hcp[8])
@@ -676,3 +709,4 @@ elif mode == 'analyze_results':
     output_file = open('results','w')
     output_file.write(output_all)
     output_file.close()
+    
